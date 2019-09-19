32963df75b8634d7c4682ed8de802b1021cb537a
Victor Domingos <editor.arcosonline@gmail.com>
2019-09-19 11:12:00 +0100

    hello world

---
+++ 01_early_test/webforms_exercise/WebForm1.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="webforms_exercise.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>Hello, my dear world!
+            <asp:Button ID="btn_click_me" runat="server" Text="Click me!" OnClick="btn_click_me_Click" />
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ 01_early_test/webforms_exercise/WebForm1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace webforms_exercise
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_click_me_Click(object sender, EventArgs e)
+        {
+            btn_click_me.Text = "Oh, no! Please stop!...";
+        }
+    }
+}
