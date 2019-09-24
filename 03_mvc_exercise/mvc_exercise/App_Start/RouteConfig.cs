12d40780794a552cb770b194bd1a26497b639831
Victor Domingos <editor.arcosonline@gmail.com>
2019-09-24 11:18:48 +0100

    viewbag

--- 03_mvc_exercise/mvc_exercise/App_Start/RouteConfig.cs
+++ 03_mvc_exercise/mvc_exercise/App_Start/RouteConfig.cs
@@ -8,7 +8,7 @@ using System.Web.Routing;
 namespace mvc_exercise
 {
     public class RouteConfig
-    {
+    {                       
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
---
+++ 03_mvc_exercise/mvc_exercise/Controllers/PassingValuesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_exercise.Controllers
+{
+    public class PassingValuesController : Controller
+    {
+        // GET: PassingValues
+        public ActionResult Index()
+        {
+            ViewBag.X = "hello, everyone!";
+            return View();
+        }
+    }
+}
