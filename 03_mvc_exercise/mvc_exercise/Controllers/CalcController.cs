3dda159b07a4632c806f9eff94a3c94e9ccc7ffc
Victor Domingos <editor.arcosonline@gmail.com>
2019-09-24 12:51:22 +0100

    form

---
+++ 03_mvc_exercise/mvc_exercise/Controllers/CalcController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_exercise.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        public ActionResult Index(string quantity, string price)
+        {
+
+            bool status;
+            ViewBag.result = 0;
+
+            if (string.IsNullOrEmpty(quantity))
+            {
+                ViewBag.qtyMsg = "Please insert quantity.";
+            }
+            else
+            {
+                ViewBag.qtyMsg = "";
+            }
+            if(string.IsNullOrEmpty(price))
+            {
+                ViewBag.priceMsg = "Please insert price.";
+            }
+            else
+            {
+                ViewBag.priceMsg = "";
+            }
+
+
+            status = float.TryParse(quantity, out float f_quantity);
+            if(status && float.TryParse(quantity, out float f_price))
+            {
+                ViewBag.result = f_quantity * f_price;
+            }
+                        
+            return View();
+        }
+    }
+}
