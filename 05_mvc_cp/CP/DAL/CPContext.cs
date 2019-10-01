8d5975ed0aeab439820c084b0f381e815bca004d
Victor Domingos <editor.arcosonline@gmail.com>
2019-10-01 09:54:09 +0100

    DAL + Model

---
+++ 05_mvc_cp/CP/DAL/CPContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using CP.Models;
+using System.Data.Entity;
+
+namespace CP.DAL
+{
+    public class CPContext
+    {
+        public DbSet<Categoria> Tcategorias { get; set; }
+
+    }
+}
