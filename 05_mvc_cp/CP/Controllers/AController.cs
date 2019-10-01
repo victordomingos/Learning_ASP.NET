f7ac89b49319df66d26eb90297632e01d5e7edd5
Victor Domingos <editor.arcosonline@gmail.com>
2019-10-01 11:20:27 +0100

    basic info retrieval from controller

---
+++ 05_mvc_cp/CP/Controllers/AController.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using CP.Models;
+using CP.DAL;
+
+namespace CP.Controllers
+{
+    public class AController : Controller
+    {
+        CPContext dbPointer = new CPContext();
+
+        // GET: A
+        public ActionResult Index()
+        {
+            List<Categoria> listagem = dbPointer.Tcategorias.ToList();
+            ViewBag.LISTAGEM = listagem;
+
+            return View();
+        }
+    }
+}
