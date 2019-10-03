da163021578fa16ebfdf39d81e25b037b76bff40
Victor Domingos <editor.arcosonline@gmail.com>
2019-10-03 15:27:11 +0100

    Acerca

---
+++ 06_mvc_pe/PE/Controllers/AcercaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PE.Controllers
+{
+    public class AcercaController : Controller
+    {
+        // GET: Acerca
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
