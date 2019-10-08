70eb5658e8bc3c51a5956cffc192e5a9a89e39f9
Victor Domingos <editor.arcosonline@gmail.com>
2019-10-08 09:56:46 +0100

    A - DropList com contrudo estático

---
+++ 07_mvc_seq_exercicios_equipas/EM_2019b/Controllers/AController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EquipaMembros2019.Controllers
+{
+    public class AController : Controller
+    {
+        // GET: A
+        public ActionResult Index()
+        {
+            var listaDeNomes = new List<string>();
+            listaDeNomes.Add("F. C. Porto");
+            listaDeNomes.Add("S. L. Benfica");
+            listaDeNomes.Add("Belenenses");
+            listaDeNomes.Add("Atlético de Valdevez");
+            listaDeNomes.Add("ADECAS");
+            listaDeNomes.Add("Desportivo das Bananas");
+            listaDeNomes.Add("Grupo Desportivo Tomate Feroz");
+            listaDeNomes.Sort();
+
+            ViewBag.LISTA = new SelectList(listaDeNomes);
+
+            return View();
+        }
+    }
+}
