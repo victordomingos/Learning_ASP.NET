4ada652dc60f8dcadc844a124973115cb7122f9a
Victor Domingos <editor.arcosonline@gmail.com>
2019-10-08 10:25:32 +0100

    B - foreach razor

---
+++ 07_mvc_seq_exercicios_equipas/EM_2019b/Controllers/BController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EquipaMembros2019.DAL;
+using EquipaMembros2019.Models;
+
+namespace EquipaMembros2019.Controllers
+{
+    public class BController : Controller
+    {
+        private EquipasContext db = new EquipasContext();
+
+        // GET: B
+        public ActionResult Index()
+        {
+            List<Equipa> listaDeEquipas = db.Tequipas.ToList();
+            ViewBag.EQUIPAS = listaDeEquipas;
+            return View();
+        }
+        
+
+    }
+}
