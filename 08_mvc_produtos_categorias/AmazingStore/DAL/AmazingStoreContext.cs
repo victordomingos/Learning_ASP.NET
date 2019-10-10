6fa8184903a7a4503ac611282b5d59100eab2251
Victor Domingos <editor.arcosonline@gmail.com>
2019-10-10 13:28:36 +0100

    08_mvc_produtos_categorias

---
+++ 08_mvc_produtos_categorias/AmazingStore/DAL/AmazingStoreContext.cs
@@ -0,0 +1,16 @@
+﻿using AmazingStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AmazingStore.DAL
+{
+    public class AmazingStoreContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+    }
+}
---
+++ 08_mvc_produtos_categorias/AmazingStore/Models/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AmazingStore.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        [Display(Name="Produto")]
+        [Required(ErrorMessage ="Deve indicar um nome para o produto.")]
+        public string Nome { get; set; }
+
+        public int QtdStock { get; set; }
+        
+        public int Preco { get; set; }
+
+        [Required(ErrorMessage = "Deve selecionar uma categoria para o produto.")]
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+
+    }
+}
