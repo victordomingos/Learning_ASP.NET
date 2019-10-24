3ecab49d4370a723a30f9dab133bfe003c571b37
Victor Domingos <editor.arcosonline@gmail.com>
2019-10-24 11:43:29 +0100

    filtros - exercicios

---
+++ 09_MVC_Projeto_W/projW/Controllers/LinhasDeTarefaPorIdTarefaController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using projW.DAL;
+using projW.Models;
+
+namespace projW.Controllers
+{
+    public class LinhasDeTarefaPorIdTarefaController : Controller
+    {
+        private victor_DbGesTarefas db = new victor_DbGesTarefas();
+
+        // GET: LinhasDeTarefaPorIdTarefa
+        public ActionResult Index(string tarefaId)
+        {
+            
+            var linhaDeTarefa = db.LinhaDeTarefa.Include(l => l.Tarefa);
+
+            if (Int32.TryParse(tarefaId, out int id))
+            {
+                linhaDeTarefa = linhaDeTarefa.Where(l => l.TarefaID == id);
+                ViewBag.TAREFAID = tarefaId;
+            }
+
+            return View(linhaDeTarefa.ToList());
+        }
+
+        // GET: LinhasDeTarefaPorIdTarefa/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LinhaDeTarefa linhaDeTarefa = db.LinhaDeTarefa.Find(id);
+            if (linhaDeTarefa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(linhaDeTarefa);
+        }
+
+        // GET: LinhasDeTarefaPorIdTarefa/Create
+        public ActionResult Create()
+        {
+            ViewBag.TarefaID = new SelectList(db.Tarefas, "ID", "Titulo");
+            return View();
+        }
+
+        // POST: LinhasDeTarefaPorIdTarefa/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Descritivo,DataDaLinha,TarefaID")] LinhaDeTarefa linhaDeTarefa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.LinhaDeTarefa.Add(linhaDeTarefa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.TarefaID = new SelectList(db.Tarefas, "ID", "Titulo", linhaDeTarefa.TarefaID);
+            return View(linhaDeTarefa);
+        }
+
+        // GET: LinhasDeTarefaPorIdTarefa/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LinhaDeTarefa linhaDeTarefa = db.LinhaDeTarefa.Find(id);
+            if (linhaDeTarefa == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TarefaID = new SelectList(db.Tarefas, "ID", "Titulo", linhaDeTarefa.TarefaID);
+            return View(linhaDeTarefa);
+        }
+
+        // POST: LinhasDeTarefaPorIdTarefa/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Descritivo,DataDaLinha,TarefaID")] LinhaDeTarefa linhaDeTarefa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(linhaDeTarefa).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.TarefaID = new SelectList(db.Tarefas, "ID", "Titulo", linhaDeTarefa.TarefaID);
+            return View(linhaDeTarefa);
+        }
+
+        // GET: LinhasDeTarefaPorIdTarefa/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LinhaDeTarefa linhaDeTarefa = db.LinhaDeTarefa.Find(id);
+            if (linhaDeTarefa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(linhaDeTarefa);
+        }
+
+        // POST: LinhasDeTarefaPorIdTarefa/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            LinhaDeTarefa linhaDeTarefa = db.LinhaDeTarefa.Find(id);
+            db.LinhaDeTarefa.Remove(linhaDeTarefa);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ 09_MVC_Projeto_W/projW/Controllers/TarefasPorClienteController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using projW.DAL;
+using projW.Models;
+
+namespace projW.Controllers
+{
+    public class TarefasPorClienteController : Controller
+    {
+        private victor_DbGesTarefas db = new victor_DbGesTarefas();
+
+        // GET: TarefasPorCliente
+        public ActionResult Index(int? ClienteID)
+        {
+            List<Cliente> listaClientes = db.Clientes.ToList();
+            var sLista = new SelectList(listaClientes, "Id", "NomeCliente");
+            ViewBag.CLIENTES = sLista;
+
+
+
+            var tarefas = db.Tarefas.Include(t => t.Cliente)
+                    .Include(t => t.Funcionario)
+                    .Include(t => t.TipoPrioridade)
+                    .Include(t => t.TipoTarefa);
+
+            if (!ClienteID.HasValue)
+            {
+                tarefas = tarefas.Where(t => t.Cliente.Id == ClienteID);
+            }
+
+
+            ViewBag.TOTAL = tarefas.Count();
+            ViewBag.CLIENTEID = ClienteID;
+            
+            return View(tarefas.ToList());
+        }
+
+        // GET: TarefasPorCliente/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarefa tarefa = db.Tarefas.Find(id);
+            if (tarefa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarefa);
+        }
+
+        // GET: TarefasPorCliente/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClienteID = new SelectList(db.Clientes, "Id", "NomeCliente");
+            ViewBag.FuncionarioID = new SelectList(db.Funcionarios, "Id", "NomeFuncionario");
+            ViewBag.TipoPrioridadeID = new SelectList(db.TiposDePrioridade, "Id", "DesignacaoPrioridade");
+            ViewBag.TipoTarefaID = new SelectList(db.TiposDeTarefa, "Id", "DesignacaoTipoTarefa");
+            return View();
+        }
+
+        // POST: TarefasPorCliente/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Titulo,Equipa,DataRegisto,DataLimite,SujeitaCoima,Descritivo,Estado,ClienteID,TipoPrioridadeID,TipoTarefaID,FuncionarioID")] Tarefa tarefa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tarefas.Add(tarefa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ClienteID = new SelectList(db.Clientes, "Id", "NomeCliente", tarefa.ClienteID);
+            ViewBag.FuncionarioID = new SelectList(db.Funcionarios, "Id", "NomeFuncionario", tarefa.FuncionarioID);
+            ViewBag.TipoPrioridadeID = new SelectList(db.TiposDePrioridade, "Id", "DesignacaoPrioridade", tarefa.TipoPrioridadeID);
+            ViewBag.TipoTarefaID = new SelectList(db.TiposDeTarefa, "Id", "DesignacaoTipoTarefa", tarefa.TipoTarefaID);
+            return View(tarefa);
+        }
+
+        // GET: TarefasPorCliente/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarefa tarefa = db.Tarefas.Find(id);
+            if (tarefa == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClienteID = new SelectList(db.Clientes, "Id", "NomeCliente", tarefa.ClienteID);
+            ViewBag.FuncionarioID = new SelectList(db.Funcionarios, "Id", "NomeFuncionario", tarefa.FuncionarioID);
+            ViewBag.TipoPrioridadeID = new SelectList(db.TiposDePrioridade, "Id", "DesignacaoPrioridade", tarefa.TipoPrioridadeID);
+            ViewBag.TipoTarefaID = new SelectList(db.TiposDeTarefa, "Id", "DesignacaoTipoTarefa", tarefa.TipoTarefaID);
+            return View(tarefa);
+        }
+
+        // POST: TarefasPorCliente/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Titulo,Equipa,DataRegisto,DataLimite,SujeitaCoima,Descritivo,Estado,ClienteID,TipoPrioridadeID,TipoTarefaID,FuncionarioID")] Tarefa tarefa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tarefa).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClienteID = new SelectList(db.Clientes, "Id", "NomeCliente", tarefa.ClienteID);
+            ViewBag.FuncionarioID = new SelectList(db.Funcionarios, "Id", "NomeFuncionario", tarefa.FuncionarioID);
+            ViewBag.TipoPrioridadeID = new SelectList(db.TiposDePrioridade, "Id", "DesignacaoPrioridade", tarefa.TipoPrioridadeID);
+            ViewBag.TipoTarefaID = new SelectList(db.TiposDeTarefa, "Id", "DesignacaoTipoTarefa", tarefa.TipoTarefaID);
+            return View(tarefa);
+        }
+
+        // GET: TarefasPorCliente/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarefa tarefa = db.Tarefas.Find(id);
+            if (tarefa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarefa);
+        }
+
+        // POST: TarefasPorCliente/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tarefa tarefa = db.Tarefas.Find(id);
+            db.Tarefas.Remove(tarefa);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
