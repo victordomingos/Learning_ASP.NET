3c031c60eb4910c59d3d8eca04b470fc7b533abf
Victor Domingos <editor.arcosonline@gmail.com>
2019-11-11 13:25:01 +0000

    Está a dar erro :-P

---
+++ 09_MVC_Projeto_W/projW/Controllers/TarefasPorFuncionarioMesAtualComViewModelController.cs
@@ -0,0 +1,27 @@
+﻿using projW.DAL;
+using projW.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projW.Controllers
+{
+    public class TarefasPorFuncionarioMesAtualComViewModelController : Controller
+    {
+        private victor_DbGesTarefas db = new victor_DbGesTarefas();
+
+        // GET: TarefasPorFuncionarioMesAtualComViewModel
+        public ActionResult Index(int? id_funcionario, int? id_tarefa)
+        {
+            var viewModel = new FuncionarioTarefas();
+            ViewBag.FUNCIONARIOS = new SelectList(db.Funcionarios, "Id", "NomeFuncionario");
+
+            viewModel.Tarefas = db.Tarefas.Where(t => t.FuncionarioID == id_funcionario);
+            viewModel.LinhasDeTarefa = db.LinhaDeTarefa.Where(l => l.TarefaID == id_tarefa);
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ 09_MVC_Projeto_W/projW/ViewModel/FuncionarioTarefas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using projW.Models;
+
+
+namespace projW.ViewModel
+{
+    public class FuncionarioTarefas
+    {
+        public IEnumerable<Funcionario> Funcionarios { get; set; }
+        public IEnumerable<Tarefa> Tarefas { get; set; }
+        public IEnumerable<LinhaDeTarefa> LinhasDeTarefa { get; set; }
+    }
+}
